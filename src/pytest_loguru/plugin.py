5694e04833ce53d347dd2548beb061e897bc4712
Mike <rans@un.org>
2022-02-28 09:10:02 +1300

    First attempt

---
+++ src/pytest_loguru/plugin.py
@@ -0,0 +1,20 @@
+import pytest
+from _pytest.logging import LogCaptureFixture
+
+
+@pytest.fixture
+def caplog(caplog: LogCaptureFixture) -> None:
+    """Emitting logs from loguru's logger.log means that they will not show up in
+    caplog which only works with Python's standard logging. This adds the same
+    LogCaptureHandler being used by caplog to hook into loguru.
+
+    Args:
+        caplog (LogCaptureFixture): caplog fixture
+
+    Returns:
+        None
+    """
+
+    handler_id = logger.add(caplog.handler, format="{message}")
+    yield caplog
+    logger.remove(handler_id)
