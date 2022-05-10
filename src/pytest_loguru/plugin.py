3ba62c657f3ff485ef6cf3f72416e0895ff36b1d
Mike <rans@un.org>
2022-05-10 14:41:30 +1200

    Fix error: "recursive dependency involving fixture 'caplog' detected" with def caplog(caplog: LogCaptureFixture)...

--- src/pytest_loguru/plugin.py
+++ src/pytest_loguru/plugin.py
@@ -1,26 +1,29 @@
 import pytest  # pragma: no cover
-from _pytest.logging import LogCaptureFixture  # pragma: no cover
+from _pytest.fixtures import SubRequest  # pragma: no cover
 from loguru import logger  # pragma: no cover
 
 
 @pytest.fixture
-def caplog(caplog: LogCaptureFixture) -> None:
+def caplog(request: SubRequest) -> None:
     """Emitting logs from loguru's logger.log means that they will not show up in
     caplog which only works with Python's standard logging. This adds the same
     LogCaptureHandler being used by caplog to hook into loguru.
 
     Args:
-        caplog (LogCaptureFixture): caplog fixture
+        request (SubRequest): SubRequest fixture to look up fixture
 
     Returns:
         None
     """
 
+    orig_caplog = request.getfixturevalue("caplog")
+
     def filter_(record):
-        return record["level"].no >= caplog.handler.level
+        return record["level"].no >= orig_caplog.handler.level
+
 
     handler_id = logger.add(
-        caplog.handler, level=0, format="{message}", filter=filter_
+        orig_caplog.handler, level=0, format="{message}", filter=filter_
     )
-    yield caplog
+    yield orig_caplog
     logger.remove(handler_id)
