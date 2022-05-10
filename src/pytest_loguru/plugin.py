3cde9efff9f395ed66f3b7def8ebe4a29a809bf5
Mike <rans@un.org>
2022-05-10 15:17:16 +1200

    Revert changes

--- src/pytest_loguru/plugin.py
+++ src/pytest_loguru/plugin.py
@@ -1,28 +1,26 @@
 import pytest  # pragma: no cover
-from _pytest.fixtures import SubRequest  # pragma: no cover
+from _pytest.logging import LogCaptureFixture  # pragma: no cover
 from loguru import logger  # pragma: no cover
 
 
 @pytest.fixture
-def caplog(request: SubRequest) -> None:
+def caplog(caplog: LogCaptureFixture) -> None:
     """Emitting logs from loguru's logger.log means that they will not show up in
     caplog which only works with Python's standard logging. This adds the same
     LogCaptureHandler being used by caplog to hook into loguru.
 
     Args:
-        request (SubRequest): SubRequest fixture to look up fixture
+        caplog (LogCaptureFixture): caplog fixture
 
     Returns:
         None
     """
 
-    orig_caplog = request.getfixturevalue("caplog")
-
     def filter_(record):
-        return record["level"].no >= orig_caplog.handler.level
+        return record["level"].no >= caplog.handler.level
 
     handler_id = logger.add(
-        orig_caplog.handler, level=0, format="{message}", filter=filter_
+        caplog.handler, level=0, format="{message}", filter=filter_
     )
-    yield orig_caplog
+    yield caplog
     logger.remove(handler_id)
