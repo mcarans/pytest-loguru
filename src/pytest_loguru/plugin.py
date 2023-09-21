b184299e85e246b0f173d4f48e52a7df017e04e0
Paweł Lis <pawel.lis@snowflake.com>
2023-09-21 16:46:54 +0200

    Fix type hints

--- src/pytest_loguru/plugin.py
+++ src/pytest_loguru/plugin.py
@@ -1,10 +1,11 @@
 import pytest  # pragma: no cover
 from _pytest.logging import LogCaptureFixture  # pragma: no cover
 from loguru import logger  # pragma: no cover
+from typing import Iterator # pragma: no cover
 
 
 @pytest.fixture
-def caplog(caplog: LogCaptureFixture) -> None:
+def caplog(caplog: LogCaptureFixture) -> Iterator[LogCaptureFixture]:
     """Emitting logs from loguru's logger.log means that they will not show up in
     caplog which only works with Python's standard logging. This adds the same
     LogCaptureHandler being used by caplog to hook into loguru.
@@ -12,8 +13,8 @@ def caplog(caplog: LogCaptureFixture) -> None:
     Args:
         caplog (LogCaptureFixture): caplog fixture
 
-    Returns:
-        None
+    Yields:
+        LogCaptureFixture
     """
 
     def filter_(record):
