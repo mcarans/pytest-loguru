13be4c3f8cb2709e7e3fa3f4085169c622452040
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2026-09-22 23:36:38 +0400

    fix: declare pytest as a runtime dependency and use public LogCaptureFixture (#14) (#20)

--- src/pytest_loguru/plugin.py
+++ src/pytest_loguru/plugin.py
@@ -1,12 +1,13 @@
 from typing import Iterator  # pragma: no cover
 
 import pytest  # pragma: no cover
-from _pytest.logging import LogCaptureFixture  # pragma: no cover
 from loguru import logger  # pragma: no cover
 
 
 @pytest.fixture
-def caplog(caplog: LogCaptureFixture) -> Iterator[LogCaptureFixture]:
+def caplog(
+    caplog: pytest.LogCaptureFixture,
+) -> Iterator[pytest.LogCaptureFixture]:
     """Emitting logs from loguru's logger.log means that they will not show up in
     caplog which only works with Python's standard logging. This adds the same
     LogCaptureHandler being used by caplog to hook into loguru.
